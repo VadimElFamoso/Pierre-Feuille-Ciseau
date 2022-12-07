0052d003da0c99f81c503aefca46d1ad5c65cd48
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-12-07 23:21:49 +0100

    Commit du 07.12.22

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const difficulty = document.getElementById('difficulty');
-const human_choice = document.getElementsByClassName('fa-solid')
-console.log(human_choice);
+const pierre = document.getElementsByClassName('fa-hand-back-fist');
+const feuille = document.getElementsByClassName('fa-hand');
+const ciseau = document.getElementsByClassName('fa-hand-scissors');
 
-let i = 0
-for(i ; human_choice.length - 1 ; i++){
-    human_choice[i].addEventListener('click', function(){
-        human_choice[i].style.color = 'red';
-        return;
-    })
-}
+let i = 0;
+for(i ; i>=pierre.length ; i++){
+  console.log(pierre[i]);
+  };
+
+
+//const human_choice = document.getElementById('human_container').getElementsByClassName('fa-solid');
+//const cpu_choice = document.getElementById('cpu_container').getElementsByClassName('fa-solid');
+
+
+
+
+
+
+
