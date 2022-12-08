7b685486b57828657cce3eca8b68f8a6294fa69e
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-12-08 23:12:58 +0100

    Commit du 08.12.22

--- app.js
+++ app.js
@@ -3,10 +3,51 @@ const pierre = document.getElementsByClassName('fa-hand-back-fist');
 const feuille = document.getElementsByClassName('fa-hand');
 const ciseau = document.getElementsByClassName('fa-hand-scissors');
 
-let i = 0;
-for(i ; i>=pierre.length ; i++){
-  console.log(pierre[i]);
-  };
+
+
+function humanChoice(type){
+  if(!type){
+    return console.log("Erreur : Le type fourni dans la fonction onClickDetection n'est pas valide");
+  }
+  for(let i = 0 ; i <= type.length - 1 ; i++){
+    type[i].addEventListener('click', function(){
+      type[i].style.color = 'green';
+      let triggered = true;
+    })
+  }
+}
+
+function cpuChoice(){
+  let cpu_random = Math.floor(Math.random() * 3);
+  switch (cpu_random){
+    case cpu_random = 1:
+      console.log("Pierre a été choisi par l'ordinateur");
+      
+      break;
+    case cpu_random = 2:
+      console.log("Feuille a été choisi par l'ordinateur");
+      break;
+    case cpu_random = 3:
+      console.log("Ciseau a été choisi par l'ordinateur");
+      break;
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
 
 
 //const human_choice = document.getElementById('human_container').getElementsByClassName('fa-solid');
